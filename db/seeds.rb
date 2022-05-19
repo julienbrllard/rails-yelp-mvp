7ff752c3fdd7c55e0fe078b539c2a7b8d361a523
Julien Brieallard <julien.brieallard@gmail.com>
2022-05-19 14:15:25 +0200

    exo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Restaurant.create( name: "McDo Passy", address: "Place de Passy", phone_number: "0101010101", category: "french" )
+Restaurant.create( name: "Pizza Hut", address: "5 rue de l'Annonciation", phone_number: "0136010128", category: "italian" )
+Restaurant.create( name: "Cucchiettiis", address: "1 Porte de la Chapelle", phone_number: "6666666666", category: "chinese" )
+Restaurant.create( name: "rodo's", address: "4bis rue de l'Ouest", phone_number: "0687553232", category: "french" )
+Restaurant.create( name: "Sushisse", address: "18 Avenue des Champs Elysées", phone_number: "0137290765", category: "japonese" )
